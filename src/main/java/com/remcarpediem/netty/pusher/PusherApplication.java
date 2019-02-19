1694b91fbb84f7fc7108c409dbbb0e01616199d0
Randy <ztelur@gmail.com>
2019-02-19 12:14:32 +0800

    update

---
+++ src/main/java/com/remcarpediem/netty/pusher/PusherApplication.java
@@ -0,0 +1,13 @@
+package com.remcarpediem.netty.pusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PusherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PusherApplication.class, args);
+	}
+
+}
